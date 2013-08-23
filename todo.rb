dbfa1ff0d068c3b91fa75ecc65a18a03ca86176a
Apprentice <apprentice@dbc07.local>
2013-08-23 15:44:01 -0700

    Refactor code

--- todo.rb
+++ todo.rb
@@ -1,40 +1,41 @@
 require_relative 'config/application'
 
-module Controller
+class Controller
 
   @command = ARGV.shift
   @task_item = ARGV.join(" ")
 
   def self.input
-
     case @command
     when "list"
       View.format_list(Task.all)
     when "delete"
-      task_array = Task.all
-      puts "Deleted: #{task_array[@task_item.to_i - 1][:description]}"
-      Task.delete(task_array[(@task_item.to_i) - 1 ][:id])
+      puts "Deleted: #{specific_task[:description]}"
+      Task.delete(specific_task[:id])
     when "add"
       Task.create(description: @task_item)
     when "completed"
-      task_array = Task.all
-      puts "Completed: #{task_array[@task_item.to_i - 1][:description]}"
-      Task.update(task_array[(@task_item.to_i) - 1 ][:id], :completed => true)
+      puts "Completed: #{specific_task[:description]}"
+      Task.update(specific_task[:id], :completed => true)
     else
-      "This command is incorrect."
+      p "This command is incorrect."
     end
   end
 
-  class View
+  def self.specific_task
+    task_array = Task.all
+    task_array[@task_item.to_i - 1]
+  end
+end
 
+class View
     def self.format_list(task_array)
       task_array.each_with_index do |element, index|
         puts "#{index + 1}. [#{'X' if element[:completed]}] #{element[:description]}"
       end
-
     end
-  end
 end
+
 
 
 Controller.input
