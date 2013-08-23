98854ab7f73764e67bddb0a2fea88a03c5dce550
Apprentice <apprentice@dbc07.local>
2013-08-23 15:15:51 -0700

    Complete MVP

--- db/migrate/20130822120600_create_tasks.rb
+++ db/migrate/20130822120600_create_tasks.rb
@@ -4,7 +4,7 @@ class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.string :description
-      t.boolean :completed
+      t.boolean :completed, default: false
     end
   end
 end
--- todo.rb
+++ todo.rb
@@ -1,29 +1,40 @@
 require_relative 'config/application'
 
-puts "Put your application code in #{File.expand_path(__FILE__)}"
+module Controller
 
-class Controller
-ARGV
+  @command = ARGV.shift
+  @task_item = ARGV.join(" ")
 
-case ARGV[0] 
-  when "list"
-    View.format_list(Task.all)
-  when "delete"
-    task_array = Task.all
-    puts "Deleted: #{task_array[ARGV[1]-1][:description]}"
-    Task.find(task_array[ARGV[1] - 1][:id]).destroy
-  when "add" 
+  def self.input
 
-end
-
-class View
-
-  def self.format_list(task_array)
-    task_array.each_with_index do |element, index|
-    puts "#{index + 1}. [#{if element[:completed] ? 'X' : ' '}] #{element[:description]}"
+    case @command
+    when "list"
+      View.format_list(Task.all)
+    when "delete"
+      task_array = Task.all
+      puts "Deleted: #{task_array[@task_item.to_i - 1][:description]}"
+      Task.delete(task_array[(@task_item.to_i) - 1 ][:id])
+    when "add"
+      Task.create(description: @task_item)
+    when "completed"
+      task_array = Task.all
+      puts "Completed: #{task_array[@task_item.to_i - 1][:description]}"
+      Task.update(task_array[(@task_item.to_i) - 1 ][:id], :completed => true)
+    else
+      "This command is incorrect."
+    end
   end
 
+  class View
 
+    def self.format_list(task_array)
+      task_array.each_with_index do |element, index|
+        puts "#{index + 1}. [#{'X' if element[:completed]}] #{element[:description]}"
+      end
 
-
+    end
+  end
 end
+
+
+Controller.input
